471b125224a91166e629795802f95e16c36c46e6
Jiro Delfino <jiro.delfino24@gmail.com>
2025-05-11 15:34:18 +0800

    JATT-56 ui: Add Header template

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return(
+        <header>
+            <h1>Jiro - Frontend Developer</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+            <button>Get in Touch</button>
+        </header>
+    )
+}
+export default Header;
