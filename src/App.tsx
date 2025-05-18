ec0dd5c0b44689d7220009e6850cdad48a6062de
Jiro Delfino <jiro.delfino24@gmail.com>
2025-05-19 00:09:31 +0800

    JATT-58 fix(form): Resolve missing imports and unused variable warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { ThemeProvider } from "@/components/theme-provider"
+import { ProfileForm } from '@/components/ContactForm'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className='w-screen h-screen'>
         <Header name='Jiro Delfino'/>
+        <ProfileForm />
         <Footer name='Jiro Delfino'/>
       </div>
     </ThemeProvider>
